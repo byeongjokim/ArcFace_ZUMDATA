fbe63f1b67565cb4a8f407a03b6066ed16b798f7
byeongjokim <byeongjooooo.kim@gmail.com>
2021-03-17 16:04:19 +0900

    before config

--- config/config.py
+++ config/config.py
@@ -1,41 +1,47 @@
 class Config(object):
+    env = 'default'
+    backbone = 'resnet18'
+    classify = 'softmax'
     num_classes = 200
+    metric = 'arc_margin'
     easy_margin = False
     use_se = False
-    optimizer = 'sgd'
+    loss = 'focal_loss'
 
-    # origin_root = "/home/kbj/projects/arcface-pytorch/origin/"
-    mind_origin_root = "/data/video/vod_tag02/bai/99/EBS"
-    inf_json = "/home/kbj/projects/ZUMDATA/inf/people/OBS"
-    mind_json = "./mind"
+    display = False
+    finetune = False
 
-    root = '/home/kbj/projects/ZUMDATA/total/'
-    total_list = '/home/kbj/projects/ZUMDATA/total.txt'
-    train_list = '/home/kbj/projects/ZUMDATA/train.txt'
-    val_list = '/home/kbj/projects/ZUMDATA/val.txt'
-    pair_list = '/home/kbj/projects/ZUMDATA/test_pair.txt'
+    train_root = "/home/kbj/projects/ZUMDATA/total/"
+    train_list = "/home/kbj/projects/ZUMDATA/train.txt"
+    val_list = "/home/kbj/projects/ZUMDATA/val.txt"
+    test_root = '/data1/Datasets/anti-spoofing/test/data_align_256'
+    test_list = 'test.txt'
 
-    val_per_class = 5
-    same_num = 3
-    diff_num = 2
-    
-    input_shape = (128, 128)
+    lfw_root = '/home/kbj/projects/ZUMDATA/total/'
+    lfw_test_list = '/home/kbj/projects/ZUMDATA/test_pair.txt'
 
     checkpoints_path = 'checkpoints'
-    
+    load_model_path = 'models/resnet18.pth'
+    test_model_path = 'checkpoints/resnet18_110.pth'
     save_interval = 10
-    eval_interval = 10
-    print_freq = 100
 
-    batch_size = 64
-    val_batch_size = 32
-    max_epoch = 200
+    train_batch_size = 64  # batch size
+    test_batch_size = 32
 
-    lr = 1e-1 
+    input_shape = (1, 128, 128)
+
+    optimizer = 'sgd'
+
+    use_gpu = True  # use GPU or not
+    gpu_id = '1'
+    num_workers = 4  # how many workers for loading data
+    print_freq = 100  # print info every N batch
+
+    debug_file = '/tmp/debug'  # if os.path.exists(debug_file): enter ipdb
+    result_file = 'result.csv'
+
+    max_epoch = 50
+    lr = 1e-1  # initial learning rate
     lr_step = 10
+    lr_decay = 0.95  # when val_loss increase, lr = lr*lr_decay
     weight_decay = 5e-4
-
-    gpu_id = "1"
-    num_workers = 4
-    
-
